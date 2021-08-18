50a5a5dbdf8ecb7dfdeed3a6ee972c8283e20b7f
Wouter ibens <wouter.ibens@nokia.com>
2021-08-18 21:47:14 +0200

    remove prints, use settings, remove bug, only run for python

--- pycompleter.py
+++ pycompleter.py
@@ -6,17 +6,6 @@ import importlib
 import pycompleter.parso.parso as parso
 import os.path
 
-VERSION = "3.6"
-PATHS = [
-    "/usr/lib/python38.zip",
-    "/usr/lib/python3.8",
-    "/usr/lib/python3.8/lib-dynload",
-    "/home/wibens/.local/lib/python3.8/site-packages",
-    "/usr/local/lib/python3.8/dist-packages",
-    "/usr/lib/python3/dist-packages",
-    "/home/wibens/deepfield-bootstrap/pipedream/lib",
-]
-
 
 def recursive_dict(path, value):
     if len(path) > 1:
@@ -25,8 +14,8 @@ def recursive_dict(path, value):
         return {path[0].value: value}
 
 
-def generate_full_paths(path):
-    for root in PATHS:
+def generate_full_paths(path, searchpath):
+    for root in searchpath:
         for i in range(len(path), 0, -1):
             trypath = os.path.join(root, "/".join(path[:i])) + ".py"
             if os.path.isfile(trypath):
@@ -34,12 +23,13 @@ def generate_full_paths(path):
 
 
 class ParsoVisitor:
-    def __init__(self, recursion=1):
+    def __init__(self, recursion=1, searchpath=[]):
         self.data = {}
         self.name = None
         self.current = self.data
         self.recursion = recursion
         self.stack = []
+        self.searchpath = searchpath
 
     def push(self, newcurrent):
         self.stack.append(self.current)
@@ -70,7 +60,6 @@ class ParsoVisitor:
         for name, path in zip(node.get_defined_names(), node.get_paths()):
             path_merged = map(lambda x: x.value, path)
             path_merged = ".".join(path_merged)
-            print("ImportName", name)
             if node.get_path_for_name(name) != path:
                 value = "Import", self.import_parser(path)
                 for i in path:
@@ -87,7 +76,7 @@ class ParsoVisitor:
             path = map(lambda x: x.value, node.get_path_for_name(name))
             path = ".".join(path)
             self.current[name.value] = (
-                "Import" + path,
+                "Import " + path,
                 self.import_parser(node.get_path_for_name(name)),
             )
 
@@ -103,7 +92,6 @@ class ParsoVisitor:
         self.current[node.name.value] = "(" + params + ")", None
 
     def visit_ExprStmt(self, node):
-        print(node.get_rhs())
         for var in node.get_defined_names():
             self.current[var.value] = "Variable", None
 
@@ -111,7 +99,7 @@ class ParsoVisitor:
         self.visit_Module(node)
 
     def generic_visit(self, node):
-        print("Not parsed:", node)
+        # print("Not parsed:", node)
         pass
 
     def import_parser(self, importpath):
@@ -129,32 +117,31 @@ class ParsoVisitor:
 
     def import_lib(self, importpath):
         importpath = list(map(lambda x: x.value, importpath))
-        print(importpath)
-        filename, subtree = next(generate_full_paths(importpath), (None, None))
+        filename, subtree = next(
+            generate_full_paths(importpath, self.searchpath), (None, None)
+        )
         if filename:
-            print("Should scan ", filename, subtree)
+            print("Will scan ", filename, subtree)
             filetree = ast_parser(filename, None, False)
             for s in subtree:
-                filetree = filetree[s]
+                filetree = filetree.get(s, {})
             return filetree
         else:
-            print("Error import", importpath)
+            print("Could not find import", importpath)
 
 
-def ast_parser(filename, source=None, recurse=True):
+def ast_parser(filename, source=None, recurse=True, version="3.6", searchpath=[]):
     print(__file__)
     if source is None:
         with open(filename, "r") as fn:
             source = fn.read()
-    tree = parso.parse(source, path=filename, version=VERSION, cache=True)
-    visitor = ParsoVisitor(recursion=1 * recurse)
+    tree = parso.parse(source, path=filename, version=version, cache=True)
+    visitor = ParsoVisitor(recursion=1 * recurse, searchpath=searchpath)
     visitor.visit(tree)
     return visitor.data
 
 
 class pycompleterListener(sublime_plugin.EventListener):
-    value = 5
-
     def __init__(self):
         self.errors = {}
         self.popuptimer = Timer(0.3, lambda: None)
@@ -164,8 +151,13 @@ class pycompleterListener(sublime_plugin.EventListener):
         viewdata = view.substr(Region(0, view.size()))
         filename = view.file_name() or "-"
 
-        data = ast_parser(filename, viewdata)
-        # print(data)
+        version = view.settings().get("pycompleter.version", "3.6")
+        searchpath = view.settings().get("pycompleter.path", [])
+
+        print(version)
+        print(searchpath)
+
+        data = ast_parser(filename, viewdata, version=version, searchpath=searchpath)
         return data
 
     @staticmethod
@@ -177,8 +169,10 @@ class pycompleterListener(sublime_plugin.EventListener):
         return sorted(r)
 
     def on_query_completions(self, view, prefix, locations):
-        print("I get called: {} / {}".format(prefix, locations))
         for location in locations:
+            if view.scope_name(location).split(" ", 1)[0] != "source.python":
+                return
+
             path = []
             i = location - len(prefix)
             while i > 1:
@@ -190,8 +184,6 @@ class pycompleterListener(sublime_plugin.EventListener):
                     break
 
             matches = self.build_matches(view)
-            print("matches")
-            print(matches)
             if matches is None:
                 return
 
